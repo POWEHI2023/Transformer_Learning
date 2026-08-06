b4000d95e66787b1a8ebf27846755bce52dbaf32
POWEHI2023 <zxe6b885@163.com>
2026-08-06 20:32:38 +0800

    [feat] 补充 GroupQueryAttention 的实现

--- model/zattention.py
+++ model/zattention.py
@@ -1,9 +1,14 @@
+# 当前全部 Attention 的 Mask 只支持了右侧 Padding, 对于 Decode 场景左侧 Padding 
+# DecodeAttention_MHA 支持 KV Cache 中存在左侧 Padding, 因为 Decode 时输入的序列长度为 1
+# TODO 端到端如何支持左侧 Padding ?
+
 from __future__ import annotations
 
 import math
 import torch
 from model.zposition import RoPE
 
+# Pure Multi-Head Attention
 class Attention(torch.nn.Module):
     def __init__(
         self,
@@ -182,8 +187,6 @@ class MultiQueryAttention(torch.nn.Module):
         assert self.d_model % self.n_head == 0
         self.d_head = self.d_model // self.n_head
         assert self.d_head % 2 == 0
-        # 生成 causal mask 支持 Decoder-only Transformer
-        # 不生成 causal mask 支持双向 Attention 或 Cross-Attention
         self.use_causal_mask = use_causal_mask
 
         self.input_proj = torch.nn.Linear(self.d_model, self.d_model + 2 * self.d_head)
@@ -277,7 +280,142 @@ class MultiQueryAttention(torch.nn.Module):
 
         
 class GroupQueryAttention(torch.nn.Module):
-    pass
+    '''
+    MHA: Query 的每个 Head 使用一个 KV Head, 代表 Query 的 Head 个数和 KV 是一样的
+    MQA: Query 的所有 Head 使用一个 KV Head, 代表一个 Query 只有一个 KV Head
+    GQA: Query 中的一组 Head 使用一个 KV Head, 代表 KV 的 Head 个数为 Query Head // Group
+    '''
+    def __init__(
+        self,
+        d_model: int,
+        n_head: int,
+        n_kv_head: int,
+        use_causal_mask: bool = True,
+        rope_base: float = 10_000.0,
+    ) -> None:
+        super().__init__()
+
+        assert n_kv_head > 0 and n_head > 0 and d_model > 0
+        assert d_model % n_head == 0
+        assert n_head % n_kv_head == 0
+        self.d_model = d_model
+        self.n_head = n_head
+        self.d_head = self.d_model // self.n_head
+        self.n_kv_head = n_kv_head
+        self.head_per_group = self.n_head // self.n_kv_head
+
+        self.use_causal_mask = use_causal_mask
+
+        self.kv_d_model = self.n_kv_head * self.d_head
+
+        self.input_proj = torch.nn.Linear(self.d_model, self.d_model + 2 * self.kv_d_model)
+        self.output_proj = torch.nn.Linear(self.d_model, self.d_model)
+
+        self.rope = RoPE(head_dim=self.d_head, base=rope_base)
+
+    def forward(
+        self,
+        x: torch.Tensor,    # [B, S, d_model]
+        position_ids: torch.Tensor,
+        attention_mask: torch.Tensor | None = None,
+        *,
+        past_key_value: tuple[torch.Tensor, torch.Tensor] | None = None,
+        use_cache: bool = False,
+    ) -> tuple[
+        torch.Tensor,
+        tuple[torch.Tensor, torch.Tensor] | None,
+    ]:
+        batch_size, sequence_length, d_model = x.shape
+        assert d_model == self.d_model
+
+        qkv: torch.Tensor = self.input_proj(x) # [B, S, d_model + kv_d_model + kv_d_model]
+        q, k, v = qkv.split(
+            [self.d_model, self.kv_d_model, self.kv_d_model],
+            dim=-1,
+        )
+
+        # [B, H, S, D]
+        q = q.view(batch_size, sequence_length, self.n_head, self.d_head).transpose(1, 2)
+        # [B, G, S, D]
+        k = k.view(batch_size, sequence_length, self.n_kv_head, self.d_head).transpose(1, 2)
+        v = v.view(batch_size, sequence_length, self.n_kv_head, self.d_head).transpose(1, 2)
+
+        # append position for each position
+        q = self.rope(q, position_ids)
+        k = self.rope(k, position_ids)
+
+        if past_key_value is not None:
+            # in this case, x can be chunked request, so the S uppon means chunked sequence length
+            # make chunked sequence length as `ChunkS`
+
+            # [B, G, HistoryS, D]
+            k_cached, v_cached = past_key_value
+            assert k_cached.ndim == 4 and v_cached.ndim == 4
+            assert k_cached.shape == v_cached.shape
+            assert k_cached.shape[:2] == (batch_size, self.n_kv_head)
+            assert k_cached.shape[-1] == self.d_head
+
+            # History Sequence Length
+            past_length = k_cached.shape[2]
+
+            # [B, G, HistoryS + ChunkS, D]
+            k = torch.cat([k_cached, k], dim=-2)
+            v = torch.cat([v_cached, v], dim=-2)
+        # k is RoPEd now, and we need causal mask and attention mask later
+        else: past_length = 0
+
+        q_len, k_len = q.shape[2], k.shape[2]
+        query_positions = past_length + torch.arange(q_len, device=q.device)
+        key_positions = torch.arange(k_len, device=q.device)
+
+        blocked_mask = (
+            key_positions[None, :] > query_positions[:, None]
+            if self.use_causal_mask
+            else None
+        )
+
+        if attention_mask is not None:
+            assert attention_mask.shape == (batch_size, k_len)
+            valid_keys = attention_mask.to(device=q.device, dtype=torch.bool)
+            if not valid_keys.any(dim=-1).all():
+                raise ValueError("Every sample must contain at least one valid key")
+
+            # [B, 1, 1, K_len]
+            attention_mask = ~attention_mask.to(device=q.device, dtype=torch.bool)[:, None, None, :]
+            blocked_mask = (
+                attention_mask  # [B, 1, 1, K_len]
+                if blocked_mask is None
+                else blocked_mask | attention_mask  # [B, 1, Q, K_len]
+            )
+
+        local_outputs = []
+        for group_index in range(self.n_kv_head):
+            q_head_start = group_index * self.head_per_group
+            q_head_end = q_head_start + self.head_per_group
+
+            # [B, HeadPerGroup, S, D]
+            q_local: torch.Tensor = q[:, q_head_start : q_head_end]
+            # [B, 1, HS + CS, D]
+            k_local: torch.Tensor = k[:, group_index : group_index + 1]
+            v_local: torch.Tensor = v[:, group_index : group_index + 1]
+
+            # [B, HeadPerGroup, S, D] @ [B, 1, D, HS + CS] -> [B, HeadPerGroup, S, HS + CS]
+            s_local = q_local @ k_local.transpose(-2, -1) / math.sqrt(self.d_head)
+
+            if blocked_mask is not None:
+                s_local = s_local.masked_fill(blocked_mask, float("-inf"))
+
+            # [B, HeadPerGroup, S, HS + CS] @ [B, 1, HS + CS, D] -> [B, HeadPerGroup, S, D]
+            attention_weights = torch.softmax(s_local, dim=-1)
+            o_local = attention_weights @ v_local
+            local_outputs.append(o_local)
+
+        # [B, n_head, S, d_head]
+        o = torch.cat(local_outputs, dim=1)
+        o = o.transpose(1, 2).reshape(batch_size, sequence_length, self.d_model)
+
+        return self.output_proj(o), (k, v) if use_cache else None
+
 
 class InferAttention_MLA(torch.nn.Module):
     pass
