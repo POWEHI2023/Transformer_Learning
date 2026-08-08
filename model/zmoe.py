05dc92b57914cb0a75717fe42b9dd62868270de8
POWEHI2023 <zxe6b885@163.com>
2026-08-08 23:13:33 +0800

    [feat] MoE 增加 Expert Major 的计算过程, 增加 RoutingPlan 便于后续 Batched/Grouped GEMM.

--- model/zmoe.py
+++ model/zmoe.py
@@ -132,6 +132,16 @@ class TopKRouter(torch.nn.Module):
             ),
         )
 
+@dataclass
+class RoutingPlan:
+    sorted_token_indices: torch.Tensor  # token, [R]
+    sorted_expert_indices: torch.Tensor # expert, [R]
+    sorted_weights: torch.Tensor        # weight, [R]
+    tokens_per_expert: torch.Tensor     # length, [E]
+    expert_offsets: torch.Tensor        # offsets, [E + 1]
+
+    token_num: int
+
 # 介绍一下 GEMM, 通用矩阵乘法, 如果在 for 循环中计算很多个 Expert 会导致启动很多个计算 Kernel
 # 效率很差, 因此 Group GEMM 可以在 GPU Kernel 内部统一调度这些矩阵乘法提高效率,
 # Batched GEMM 通常要求每组矩阵乘法形状相同， 所有的 Expert 必须有相同的 Token 数量,
@@ -197,11 +207,74 @@ class TopKSparseMoE(torch.nn.Module):
         # [T, K], [T, K]
         router_output: RouterOutput = self.router(valid_x)
 
+        ### Spec ---------- Expert Major, Router Table
+
+        token_num, top_k = router_output.expert_indices.shape
+        route_token_indices = (
+            torch.arange(token_num, device=valid_x.device)
+            .unsqueeze(1)
+            .expand(token_num, top_k)
+            .reshape(-1)
+        )
+        route_expert_indices = router_output.expert_indices.reshape(-1)
+        route_weights = router_output.expert_weights.reshape(-1)
+
+        # 这里先把输入的 token 根据 expert id 排序,
+        # 后续先还原 token major, 然后根据 valid_indices 还原 flat_output
+        order = torch.argsort(route_expert_indices)
+        sorted_expert_indices = route_expert_indices[order]
+        sorted_token_indices = route_token_indices[order]
+        sorted_weight = route_weights[order]
+
+        # 由于[T, D] 根据 [T, K] 为 K 个 Expert Flatten 之后, 有[T * K] 个 token,
+        # 其中存在重复的 token, sorted_x 的形状是 [T * K, D].
+        sorted_x = valid_x.index_select(dim=0, index=sorted_token_indices)
+        # 构造 Expert Offsets
+        tokens_per_expert = \
+            torch.bincount(sorted_expert_indices, minlength=self.config.expert_num)
+        expert_offsets = torch.cat([
+            tokens_per_expert.new_zeros(1),
+            tokens_per_expert.cumsum(dim=0),
+        ])
+        ### Spec ---------- Expert Major, Router Table
+
+        # valid_output = self._dispatch_v1(valid_x=valid_x, router_output=router_output)
+        valid_output = self._dispatch_v2(
+            sorted_x=sorted_x,
+            routing_plan=RoutingPlan(
+                sorted_token_indices=sorted_token_indices,
+                sorted_expert_indices=sorted_expert_indices,
+                sorted_weights=sorted_weight,
+                tokens_per_expert=tokens_per_expert,
+                expert_offsets=expert_offsets,
+                token_num=valid_x.shape[0],
+            )
+        )
+
+        # valid_output 中只记录了 mask 之后不包含 PAD 的有效 token,
+        # 需要把有效 token 映射回原本的总 token 空间中.
+        flat_output = torch.zeros_like(flat_x)
+        flat_output = flat_output.index_copy(dim=0, index=valid_indices, source=valid_output)
+
+        output = flat_output.reshape(batch_size, sequence_length, self.d_model)
+        output = self.dropout(output)
+
+        # Token FFN 结果和 Router 的状态和损失
+        return FFNOutput(
+            hidden_states=output,
+            aux_loss=router_output.aux_loss,
+            z_loss=router_output.z_loss,
+            router_stats=router_output.stats,
+        )
+
+    def _dispatch_v1(
+            self,
+            valid_x: torch.Tensor,
+            router_output: RouterOutput,
+        ) -> torch.Tensor:
         # build buffer for experts' output, shape is [T, D]
         # 累加所有 Expert 的加权结果
         valid_output = torch.zeros_like(valid_x)
-
-        # !!! dispatch
         for expert_index, expert in enumerate(self.experts):
             # 每个 Token 的 K 个 Expert ID 中是否存在等于 expert_index 的 ID
             selected = (router_output.expert_indices == expert_index)
@@ -227,19 +300,35 @@ class TopKSparseMoE(torch.nn.Module):
 
             # 缓存加权结果: valid_output[token_indices[i]] += weighted_output[i]
             valid_output.index_add_(dim=0, index=token_indices, source=weighted_output)
+        return valid_output
 
-        # valid_output 中只记录了 mask 之后不包含 PAD 的有效 token,
-        # 需要把有效 token 映射回原本的总 token 空间中.
-        flat_output = torch.zeros_like(flat_x)
-        flat_output = flat_output.index_copy(dim=0, index=valid_indices, source=valid_output)
+    def _dispatch_v2(
+        self,
+        sorted_x: torch.Tensor,
+        routing_plan: RoutingPlan,
+    ) -> torch.Tensor:
+        sorted_output = torch.zeros_like(sorted_x)
+        for expert_index, expert in enumerate(self.experts):
+            start = routing_plan.expert_offsets[expert_index]
+            end = routing_plan.expert_offsets[expert_index + 1]
 
-        output = flat_output.reshape(batch_size, sequence_length, self.d_model)
-        output = self.dropout(output)
+            expert_input = sorted_x[start : end]
+            if expert_input.shape[0] == 0:
+                continue
+            sorted_output[start : end] = expert(expert_input)
 
-        # Token FFN 结果和 Router 的状态和损失
-        return FFNOutput(
-            hidden_states=output,
-            aux_loss=router_output.aux_loss,
-            z_loss=router_output.z_loss,
-            router_stats=router_output.stats,
+        sorted_weights = routing_plan.sorted_weights.to(dtype=sorted_output.dtype)
+        # [T * K, D], 根据 Expert 排列的
+        weighted_output = sorted_output * sorted_weights.unsqueeze(-1)
+        _tk, _d = weighted_output.shape
+        assert _tk // self.config.top_k == routing_plan.token_num
+        # 从 routing plan 还原 order
+        valid_output = torch.zeros(
+            (routing_plan.token_num, _d),
+            dtype=weighted_output.dtype,
+            device=weighted_output.device,
         )
+        # [T * K, D] -> [T, D]
+        valid_output.index_add_(dim=0, index=routing_plan.sorted_token_indices, source=weighted_output)
+        return valid_output
+        
